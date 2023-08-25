a15150c637bb85402717a643129831501a7bd7ab
agichimu <alexandergichimu98@gmail.com>
2023-08-24 23:06:16 -0700

    my commit on printlist task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,23 +1,27 @@
 #include "lists.h"
 
+
 /**
- * print_list - function prints all elements of a list
- * @h: pointer to singly linked list
+ * print_list - Prints every single element of a list_t list
+ * @h: pointer  to a linked list head
  * Return: returns number of elements in a list
  */
+
 size_t print_list(const list_t *h)
 {
-	size_t n_elems;
+	 size_t n_elems;
 
-	n_elems = 0;
-	while (h != NULL)
-	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
-		else
-			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		n_elems++;
-	}
-	return (n_elems);
+    n_elems = 0;
+
+    while (h != NULL)
+    {
+        if (h->str == NULL)
+            printf("[%d] %s\n", 0, "(nil)");
+        else
+            printf("[%d] %s\n", h->len, h->str);
+        h = h->next;
+        n_elems++;
+    }
+
+    return (n_elems);
 }
